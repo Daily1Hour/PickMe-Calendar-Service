ba6592851c8c4a337405c4da744eedf6fdbc48c7
wlstn <wlstn159159@naver.com>
2024-11-14 19:07:25 +0900

    refactor : 사용되지 않는 import 제거 및 코드 정리

--- src/main/java/com/example/calendar/entity/Calendar.java
+++ src/main/java/com/example/calendar/entity/Calendar.java
@@ -3,10 +3,8 @@ package com.example.calendar.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity // 해당 클래스가 데이터베이스의 테이블과 매핑
 @Data
@@ -17,7 +15,6 @@ public class Calendar {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     @Column(nullable = false)
     private String userInfo;
 
