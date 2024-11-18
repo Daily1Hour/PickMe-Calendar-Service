1d7bf4f0453bca45ada2288b8662d94104a019a7
wlstn <wlstn159159@naver.com>
2024-11-18 17:17:52 +0900

    refactor : 반환 타입을 ResponseEntity로 변경

--- src/main/java/com/example/calendar/controller/CalendarController.java
+++ src/main/java/com/example/calendar/controller/CalendarController.java
@@ -7,6 +7,8 @@ import com.example.calendar.extract.ExtractToken;
 import com.example.calendar.service.CalendarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,7 +25,7 @@ public class CalendarController {
 
     // 해당 사용자 면접 일정 전체 조회
     @GetMapping("/interviews")
-    public List<Calendar> interviewsList(@RequestHeader("Authorization") String token,
+    public ResponseEntity<?> interviewsList(@RequestHeader("Authorization") String token,
                                          @RequestParam(required = false) String position,
                                          @RequestParam(required = false) LocalDateTime startDate,
                                          @RequestParam(required = false) LocalDateTime endDate){
@@ -39,30 +41,27 @@ public class CalendarController {
         if (position == null && startDate == null && endDate == null) {
             log.info("직무와 날짜가 모두 입력되지 않았습니다.");
             // 기본 전체 데이터를 반환하거나 예외 처리
-            return calendarService.interviewsList(extractedToken);
+            return ResponseEntity.status(HttpStatus.OK).body(calendarService.interviewsList(extractedToken));
         } else if (position != null && startDate == null && endDate == null) {
             log.info("직무만 입력되었습니다: " + position);
             // 직무에 해당하는 인터뷰 데이터를 반환
-            return calendarService.interviewsListByJob(extractedToken, position);
+            return ResponseEntity.status(HttpStatus.OK).body(calendarService.interviewsListByJob(extractedToken, position));
         } else if (position == null && (startDate != null || endDate != null)) {
             log.info("날짜만 입력되었습니다: " + startDate + " ~ " + endDate);
             // 날짜 범위에 해당하는 인터뷰 데이터를 반환
-            return calendarService.interviewsListByPeriod(extractedToken, startDate, endDate);
+            return ResponseEntity.status(HttpStatus.OK).body(calendarService.interviewsListByPeriod(extractedToken, startDate, endDate));
         } else if (position != null && (startDate != null || endDate != null)) {
             log.info("직무와 날짜가 모두 입력되었습니다. 직무: " + position + ", 날짜: " + startDate + " ~ " + endDate);
             // 직무와 날짜 조건을 모두 만족하는 인터뷰 데이터를 반환
-            return calendarService.InterviewsByPositionAndDate(extractedToken, position, startDate, endDate);
+            return ResponseEntity.status(HttpStatus.OK).body(calendarService.InterviewsByPositionAndDate(extractedToken, position, startDate, endDate));
         }
 
-        // 모든 조건에 맞지 않는 경우 기본 값 반환 (필요시 추가)
-        log.warn("알 수 없는 조건으로 요청되었습니다.");
-        return Collections.emptyList();
-
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fail");
     }
 
     // 면접 일정 추가
     @PostMapping("/interviews")
-    public ResponseDto registerInterviewSchedule(@RequestHeader("Authorization") String token, @RequestBody InterviewScheduleDto interviewScheduleDto){
+    public ResponseEntity<?> registerInterviewSchedule(@RequestHeader("Authorization") String token, @RequestBody InterviewScheduleDto interviewScheduleDto){
         String extractedToken = ExtractToken.extractToken(token);
 
         // 받아온 데이터를 DB에 저장 요청
@@ -70,44 +69,39 @@ public class CalendarController {
 
         // 저장 성공 시 OK 반환
         if(b){
-
-            ResponseDto responseDto = new ResponseDto();
-            responseDto.setMessage("OK");
-            return responseDto;
+            return ResponseEntity.status(HttpStatus.OK).body("Create Success");
         }
         // 저장 실패 시 Fail 반환
-        ResponseDto responseDto = new ResponseDto();
-        responseDto.setMessage("Fail");
-        return responseDto;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
     }
 
     // 면접 일정 삭제
     @DeleteMapping("/interviews")
-    public ResponseDto deleteInterviewSchedule(@RequestHeader("Authorization") String token, @RequestParam String id){
+    public ResponseEntity<?> deleteInterviewSchedule(@RequestHeader("Authorization") String token, @RequestParam String id){
         String extractedToken = ExtractToken.extractToken(token);
         boolean b = calendarService.deleteInterviewSchedule(id);
 
         if(b){
-            ResponseDto responseDto = new ResponseDto();
-            responseDto.setMessage("OK");
-            return responseDto;
+
+            return ResponseEntity.status(HttpStatus.OK).body("Delete Success");
         }
-        ResponseDto responseDto = new ResponseDto();
-        responseDto.setMessage("Fail");
-        return responseDto;
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     // 특정 면접 일정 수정
     @PutMapping("/interviews")
-    public InterviewScheduleDto putInterviewSchedule(
+    public ResponseEntity<?> putInterviewSchedule(
             @RequestHeader("Authorization") String token,
             @RequestParam String id,
             @RequestBody InterviewScheduleDto interviewScheduleDto){
 
         String extractedToken = ExtractToken.extractToken(token);
 
-        return calendarService.putInterviewSchedule(id, interviewScheduleDto);
+        InterviewScheduleDto ResponseDto = calendarService.putInterviewSchedule(id, interviewScheduleDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto);
 
 
     }
