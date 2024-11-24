a309be09dc7f2be5719e44298e2cb9e44d40311c
wlstn <wlstn159159@naver.com>
2024-11-24 12:48:04 +0900

    feat : 면접 일정 수정 시 사용할 mapper 생성

--- src/main/java/com/pickme/calendar/service/mapper/CalendarMapper.java
+++ src/main/java/com/pickme/calendar/service/mapper/CalendarMapper.java
@@ -5,6 +5,7 @@ import com.pickme.calendar.dto.InterviewScheduleDTO;
 import com.pickme.calendar.dto.get.GetCalendarDTO;
 import com.pickme.calendar.dto.get.GetInterviewDetailDTO;
 import com.pickme.calendar.dto.post.PostInterviewDetailDTO;
+import com.pickme.calendar.dto.put.PutInterviewDetailDTO;
 import com.pickme.calendar.entity.Calendar;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -36,4 +37,8 @@ public interface CalendarMapper {
 
     // interviewDetails 리스트를 GetInterviewDetailDTO 객체로 변환
     List<GetInterviewDetailDTO> interviewDetailsToGetInterviewDetailsDTO (List<Calendar.InterviewDetails> interviewDetails);
+
+    // 전달받은 DTO(PutInterviewDetailDTO)를 InterviewDetails 객체로 변환
+    @Mapping(target = "interviewDetailId", ignore = true)
+    void putInterviewDetailDtoTOInterviewDetail (PutInterviewDetailDTO putInterviewDetailDTO, @MappingTarget Calendar.InterviewDetails interviewDetails);
 }
