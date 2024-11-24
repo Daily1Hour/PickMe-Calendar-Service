998f0df945f16e61211c4624edb94df87251980b
wlstn <wlstn159159@naver.com>
2024-11-24 13:08:23 +0900

    style : 사용하지 않는 메서드와 DTO 삭제

--- src/main/java/com/pickme/calendar/service/mapper/CalendarMapper.java
+++ src/main/java/com/pickme/calendar/service/mapper/CalendarMapper.java
@@ -1,7 +1,5 @@
 package com.pickme.calendar.service.mapper;
 
-import com.pickme.calendar.dto.GetInterviewListDTO;
-import com.pickme.calendar.dto.InterviewScheduleDTO;
 import com.pickme.calendar.dto.get.GetCalendarDTO;
 import com.pickme.calendar.dto.get.GetInterviewDetailDTO;
 import com.pickme.calendar.dto.post.PostInterviewDetailDTO;
@@ -15,17 +13,6 @@ import java.util.List;
 
 @Mapper(componentModel = "spring") // Spring Bean으로 등록
 public interface CalendarMapper {
-
-    // InterviewScheduleDto 객체를 Calendar 엔티티로 변환합니다.
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "userInfo", ignore = true)
-    void toCalendarEntity(InterviewScheduleDTO interviewScheduleDto, @MappingTarget Calendar calendar);
-
-    // Calendar 엔티티를 InterviewScheduleDto 객체로 변환합니다.
-    InterviewScheduleDTO toInterviewScheduleDto(Calendar calendar);
-
-    // Calendar 엔티티 리스트를 GetInterviewListDto 리스트로 변환합니다.
-    List<GetInterviewListDTO> toGetInterviewListDto(List<Calendar> calendarList);
 
     // 전달받은 DTO(PostInterviewDetailDTO)를 InterviewDetails 객체로 변환
     @Mapping(target = "interviewDetailId", ignore = true)
