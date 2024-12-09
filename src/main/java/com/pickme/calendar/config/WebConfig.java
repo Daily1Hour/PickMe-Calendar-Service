30dced9d8f149cb2bb4ba5679455a08a5604f587
wlstn <wlstn159159@naver.com>
2024-12-09 19:36:52 +0900

    refactor : CORS 설정 수정

--- src/main/java/com/pickme/calendar/config/WebConfig.java
+++ src/main/java/com/pickme/calendar/config/WebConfig.java
@@ -52,10 +52,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+
     }
 
 }
